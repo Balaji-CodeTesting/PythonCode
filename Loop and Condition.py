f2f420b2003c0df33dc40ba6efcc396522b70aa5
Balaji-CodeTesting <vinodhbalaji3@gmail.com>
2020-09-08 14:41:50 +0530

    Datatypes

---
+++ Loop and Condition.py
@@ -0,0 +1,24 @@
+# If Condition Program: Follow Indentation while coding
+
+a, b = 1, "India"
+if a != 1 and b != "India":
+    print("True")
+else:
+    print(b)
+
+# For Loop: Follow Indentation while coding
+
+e = [1, 2, 3, 4, 5, "Friend"]
+for d in e:
+    print(d*2)
+# To print sum of natural number
+value = 0
+for i in range(1, 10):
+    value = value + i
+print(value)
+
+# To print value by adding values by 2 (i++):
+
+for k in range(1, 8, 2):
+    print(k)
+
