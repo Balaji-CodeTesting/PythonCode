68ec7a13d8542bdc0f257cb28fcecab470f94502
Balaji-CodeTesting <vinodhbalaji3@gmail.com>
2020-09-08 15:53:42 +0530

    Logical Code syntax

---
+++ PythonFunction.py
@@ -0,0 +1,28 @@
+# In Python - Function: Group of related statements that performs a specific Task
+# Keyword used for Function: def
+
+
+def GreetingDay():
+    print("Happy Morning")
+    # To call a Function:
+
+
+GreetingDay()
+
+
+# To parameter in Function
+def GreetMe(name):
+    print("Good Morning" + name)
+    # To Call Function with parameter:
+
+
+GreetMe(" Vinodhbalaji")
+
+
+def AddInteger(a, b):
+    print(a+b)
+    # To Call Function with parameter:
+
+
+AddInteger(3, 5)
+
